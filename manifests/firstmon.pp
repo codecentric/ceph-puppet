f9ff5eb86b82435bee8ee1c954ebe4305ff7fe7f
server <server@server.local.cloud>
2014-05-08 17:32:48 +0200

    not working

--- manifests/firstmon.pp
+++ manifests/firstmon.pp
@@ -1,4 +1,4 @@
-# Anzahl / Bezeichungen der OSDs parametrisierbar machen
+# Anleitung: http://ceph.com/docs/master/install/manual-deployment/#monitor-bootstrapping
 class ceph::firstmon (
 ){
   include '::ceph'
@@ -11,23 +11,27 @@ class ceph::firstmon (
   $admin_keyring = '/etc/ceph/ceph.client.admin.keyring'
   $mon_tmp = '/tmp/monmap'
 
+# Generate ceph.conf from template
   file{ "${ceph::cluster_id}.conf":
     path     => "/etc/ceph/${ceph::cluster_id}.conf",
     content  => template('ceph/ceph.erb'),
   }
 
+# Create a keyring and generate a monitor secret key
   exec{ 'create-keyring-monitor':
     command => "ceph-authtool --create-keyring ${mon_keyring} --gen-key -n mon. --cap mon 'allow *'",
     creates => $mon_keyring,
     require => File["${ceph::cluster_id}.conf"],
   }
 
+# Generate admin keyring, generate client.admin user, add user to keyring
   exec{'create-keyring-admin':
     command  => "ceph-authtool --create-keyring ${admin_keyring} --gen-key -n client.admin --set-uid=0 --cap mon 'allow *' --cap osd 'allow *' --cap mds 'allow'",
     creates  => $admin_keyring,
     require  => Exec['create-keyring-monitor'],
   }
 
+# Add client.admin key to ceph.mon.keyring
   exec{'add-admin-to-monitor-keyring':
     command => "ceph-authtool ${mon_keyring} --import-keyring ${admin_keyring}",
     require => Exec['create-keyring-admin'],
@@ -40,18 +44,21 @@ class ceph::firstmon (
     require => Exec['add-admin-to-monitor-keyring'],
   }
 
+# Generate monitor map using hostname, host IP address and FSID. Saved as /tmp/monmap
   exec{'create-monitor-map':
     command  => "monmaptool --create --add ${hostname} ${ipaddress} --fsid ${ceph::fsid} ${mon_tmp}",
     creates  => $mon_tmp,
     require  => File['ceph.client.admin.keyring'],
   }
 
+# Create default data directory 
   file{'monitor-directory':
     ensure   => directory,
     path     => "/var/lib/ceph/mon/${ceph::cluster_id}-${hostname}",
     require  => Exec['create-monitor-map'],
   }
 
+# Populate monitor daemon with monitor map and keyring
   exec{'populate-monitor':
     command  => "ceph-mon --mkfs -i ${hostname} --monmap ${mon_tmp} --keyring ${mon_keyring}",
     require  => [
@@ -74,6 +81,7 @@ class ceph::firstmon (
 #   require  => Exec['populate-monitor'],
 # }
 
+# Start the monitor
   exec{'start-monitor':
     command  => "start ceph-mon id=${hostname}",
 #    require  => Exec['restart'],
@@ -81,6 +89,7 @@ class ceph::firstmon (
 #    unless   => "status ceph-mon id=${hostname}",
   }
 
+# Add first osd
   ceph::osd {'share1':
     directory  => '/var/local/ceph1',
     require    => [
@@ -88,6 +97,7 @@ class ceph::firstmon (
                 ],
   }
 
+# Add second osd
   ceph::osd {'share2':
     directory  => '/var/local/ceph2',
     require    => [
